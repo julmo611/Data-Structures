fdb184f360c64162bd4370a4f6f6610af3959d54
Julian Moreno <julmo611@gmail.com>
2019-06-12 14:49:39 -0700

    MVP Binary search tree done!

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -24,10 +24,29 @@ class BinarySearchTree:
                 self.right.insert(value)
 
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+
+        if target > self.value:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
 
     def get_max(self):
-        pass
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
 
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
